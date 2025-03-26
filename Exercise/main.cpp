375f4888842407de3f33293c083fe0abbd2aa7f1
Beatrice Gnai <s310738@studenti.polito.it>
2025-03-26 19:35:12 +0100

    esercizio

--- Exercise/main.cpp
+++ Exercise/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 
+using namespace std;
+
 template<typename T>
 class complex_number {
 private: 
@@ -8,93 +10,143 @@ private:
 	T Immaginaria;
 	
 public: 
-	//dichiarazione e descrizione costruttore default
+	//costruttore default
 	complex_number() 
 		: Reale(0),Immaginaria(0) 
 	{}
 	
-	//dichiarazione e descrizione costruttore parametrico
+	//costruttore parametrico
 	complex_number(T r, T i)
 		: Reale(r), Immaginaria(i)
 	{}
+
+	//metodi di accesso 
+	T getReale() const { 
+		return(Reale); 
+		}
+	T getImmaginaria() const {
+		return(Immaginaria);
+		}
 	
-	//overload operatore +
-	complex_number operator+(const complex_number& other) {
-		return complex_number(Reale + z2.Reale, Immaginaria + z2.Immaginaria);
-		
+	//coniugato 
+	complex_number coniugato()
+	const {
+		return complex_number(Reale, -Immaginaria);
+	}
+	
+	//Overload operatore + tra complessi
+	complex_number operator+(const complex_number& z2) {
+		return complex_number(Reale + z2.Reale, Immaginaria + z2.Immaginaria);   
+	}
+
+	// Overload operatore * tra complessi
+	complex_number operator*(const complex_number& z2) {
+		T newReale = Reale * z2.Reale - Immaginaria * z2.Immaginaria;
+		T newImmaginaria = Reale * z2.Immaginaria + Immaginaria * z2.Reale;
+	return complex_number(newReale, newImmaginaria);
+	}
+	
+	// Overload operatore += tra complessi
 	complex_number& operator+=(const complex_number& other) {
 		Reale += other.Reale;
 		Immaginaria += other.Immaginaria;
-	return *this;}
-		
-	complex_number operator+(const T& val) {
-	return complex_number(Reale + val, Immaginaria);}
-		
-	// Overload operatore *
-	complex_number operator*(const complex_number& other) {
-		T newReale = Reale * z2.Reale - Immaginaria * z2.Immaginaria;
-		T newImmaginaria = Reale * z2.Immaginaria + Immaginaria * z2.Reale;
-	return complex_number(newReale, newImmaginaria);}
-		
+	return *this;
+	}
 	
-	//restituzione dei valori dalla classe	
-	T getReale() const { return Reale; }
-	T getImmaginaria() const { return Immaginaria; }
+	//Overload operatore *= tra complessi
+	complex_number& operator*=(const complex_number& other) {
+		T newReale = Reale * other.Reale - Immaginaria * other.Immaginaria;
+		T newImmaginaria = Reale * other.Immaginaria + Immaginaria * other.Reale;
+		Reale =  newReale;
+		Immaginaria = newImmaginaria;
+		return *this;
+	}
 	
+	//Overload operatore +: complesso e T
+	complex_number operator+(const T& val) 
+	{
+		return complex_number(Reale + val, Immaginaria);
+	}
+	
+	//Overload operatore *: complesso e T
+	complex_number operator*(const T& val) 
+	{
+		return complex_number(Reale * val, Immaginaria * val);
+	}
 
-	
-	
 };
-	//overload operatore <<	
-    std::ostream& operator<<(std::ostream& os, const complex_number<T>& z) {
-		os << z.getReale();
-        if (z.getImmaginaria() < 0)
-			os << "-" << abs(z.getImmaginaria()) << "i";
-		else
-			os << "+" << z.getImmaginaria() << "i";
-        return os;
-		
-		
-		
- 	complex_number<T> operator+(const T& val, const  complex_number<T>& z) {
-		return complex_number(z.getReale() + val, z.getImmaginaria());
+
+//overload operatore <<	
+template<typename T>
+ostream& operator<<(ostream& output, const complex_number<T>& z) 
+{
+	output << z.getReale();
+	if (z.getImmaginaria() < 0)
+		output << "-" << abs(z.getImmaginaria()) << "i";
+	else
+		output << "+" << z.getImmaginaria() << "i";
+	return output;
+}
+
+//Overload operatore +: T e complesso 
+template<typename T> 
+complex_number<T> operator+(const T val, const complex_number<T>& z)
+{
+	return complex_number<T>(z.getReale() + val, z.getImmaginaria());
+}
+
+//Overload operatore +: T e complesso 
+template<typename T> 
+complex_number<T> operator*(const T val, const complex_number<T>& z)
+{
+	return complex_number<T>(z.getReale() * val, z.getImmaginaria() * val);
+}
+
+
 
 int main() {
-	std::cout << std::fixed << std::setprecision(2);
-	complex_number<float> z1, z2, z3;   //crea un oggetto nella classe dei complex_number
-	z1.input(); 				// input utente
-	std::cout << "La parte reale del numero complesso z1 è: " << z1.getReale() << std::endl;
-	std::cout << "La parte immaginaria del numero complesso z1 è: " << z1.getImmaginaria() << std::endl;
 	
-	//operatore << per stampare z1 
-	std::cout << "Il numero complesso è: " << z1 << std::endl;
+	using cpx = complex_number<int>;
 	
-	//coniugato
-	std::cout << "Il coniugato del numero complesso z1 è: " << z1.getReale();
-	if (z1.getImmaginaria() < 0) {
-		std::cout <<  "+" << abs(z1.getImmaginaria()) << "i" << std::endl;	
-	} else {
-		std::cout <<  "-" << z1.getImmaginaria() << "i" << std::endl;
-	};
+	cpx z1(2,5);
+	cpx z2(6,7);
 	
-	z2.input();
-	//richiesta numero complesso z2
-	std::cout << "Il numero complesso è: " << z2 << std::endl;
+	cout << "Parte Reale : " << z1.getReale() << endl;
+	cout << "Parte Immaginaria : " << z1.getImmaginaria() << endl;
+	cout << "Coniugato : " << z1.coniugato() << endl;
+	cout << "z2 : " << z2 << endl;
 	
-	//somma numeri complessi +
-	z3=z1+z2;
-	std::cout << "La somma dei numeri complessi z1 e z2 è: " << z3 << std::endl;
+	cout << "OPERAZIONI SOMMA" << endl;
+	cpx somma = z1 + z2;
+	cout << "Somma : " << somma << endl;
 	
-	// Moltiplicazione dei due numeri complessi
-	z3 = z1 * z2;
-	std::cout << "Il prodotto dei numeri complessi z1 e z2 è: " << z3 << std::endl;
+	cpx somma_c = z1 += z2;
+	cout << "Somma con += : " << somma_c << endl;
 	
+	cpx somma_C_T = z1 + 2;
+	cout << "Somma Complesso + T: " << somma_C_T << endl;
+	
+	cpx somma_T_C = 2 + z1;
+	cout << "Somma T + Complesso : " << somma_T_C << endl;
+	
+	
+	
+	cout << endl;
+	cout << "OPERAZIONI PRODOTTO" << endl;
+	cpx prodotto = z1 * z2;
+	cout << "Prodotto : " << prodotto << endl;
+	
+	cpx prodotto_c = z1 *= z2;
+	cout << "Prodotto con *= : " << prodotto_c << endl;
+	
+	cpx prodotto_C_T = z1 * 2;
+	cout << "Prodotto Complesso * T : " << prodotto_C_T << endl;
+	
+	cpx prodotto_T_C = 2 * z1;
+	cout << "Prodotto T * Complesso : " << prodotto_T_C << endl;
 
-return 0;
-	
-	
-};
-
+	return 0;
+}
 	
 
 
